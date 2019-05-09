ecb597103dd63dde4b415fc77bdff248f864a079
JP-Ellis <josh@jpellis.me>
2019-05-09 12:17:49 +1000

    Refactoring
    Signed-off-by: JP-Ellis <josh@jpellis.me>

--- game.py
+++ game.py
@@ -1,115 +1,165 @@
-import random
+"""Snake game"""
 import logging
+import random
 
 LOGGER = logging.getLogger(__name__)
 
+
 class Game:
-    def __init__(self, width, height, snake_head_pos=None):
+    """Snake game class"""
+
+    def __init__(self, width, height, snake_head_pos=None, apples=2):
+        """Initialize a snake game with the specified width and height.
+
+        The initial position of the snake's head can be specified by
+        `snake_head_pos` which should be a tuple of the form `(x, y)`.
+
+        The number of apples initially place on the board is specified by
+        `apples`.
+
+        """
         self.width = width
         self.height = height
+        self.ended = False
+
         if snake_head_pos is None:
             snake_head_pos = (width // 2, height // 2)
-        self.snake = [snake_head_pos,(snake_head_pos[0],snake_head_pos[1]+1),(snake_head_pos[0],snake_head_pos[1]+2)]
-
+        self.snake = [
+            snake_head_pos,
+            (snake_head_pos[0], snake_head_pos[1] + 1),
+            (snake_head_pos[0], snake_head_pos[1] + 2),
+        ]
         self.apples = set()
+        self.health = 100
+        self.digesting = 0
 
-        self.whole_space = set((i, j) for i in range(self.width) for j in range(self.height))
-        
-        self.make_apple()
-        self.make_apple()
+        self.whole_space = set(
+            (i, j) for i in range(self.width) for j in range(self.height)
+        )
 
-        self.food = 0
+        for _ in range(apples):
+            self.make_apple()
 
-        
+    @property
+    def snake_length(self):
+        """Length of the snake"""
+        return len(self.snake)
 
-    def empty_space(self):
-        return self.whole_space - self.apples - set(self.snake)
+    @property
+    def empty_spaces(self):
+        """Return the set of indices corresponding to empty sites."""
+        empty_spaces = self.whole_space - self.apples - set(self.snake)
+        LOGGER.debug(f"Empty spaces: {len(empty_spaces)}")
+        return empty_spaces
 
     def make_apple(self):
-        #self.apples.add((random.randint(0, self.width-1), random.randint(0, self.height-1)))
-    #make_apple()
-        self.apples.add(random.choice(tuple(self.empty_space())))
+        """Add an apple to the game"""
+        if len(self.empty_spaces) == 0:
+            LOGGER.warn(f"There are no empty spaces to place an apple.")
+            self.ended = True
+        else:
+            self.apples.add(random.choice(tuple(self.empty_spaces)))
 
-    def move_up(self):
-        if self.food == 0:
-            self.snake = [(self.snake[0][0], self.snake[0][1] - 1)] + self.snake[:-1]
-        if self.food > 0:
+    def move_up(self, costly=True):
+        """Make the snake's head up by one"""
+        if self.digesting > 0:
             self.snake = [(self.snake[0][0], self.snake[0][1] - 1)] + self.snake
-            self.food -= self.food
+            self.digesting -= 1
+        else:
+            self.snake = [(self.snake[0][0], self.snake[0][1] - 1)] + self.snake[:-1]
 
+        self.check_move()
+        if costly:
+            self.health -= 1
 
-
-
-    def move_down(self):
-        if self.food == 0:
-            self.snake = [(self.snake[0][0], self.snake[0][1] + 1)] + self.snake[:-1]
-        if self.food > 0:
+    def move_down(self, costly=True):
+        """Move the snake's head down by one"""
+        if self.digesting > 0:
             self.snake = [(self.snake[0][0], self.snake[0][1] + 1)] + self.snake
-            self.food -= self.food
+            self.digesting -= 1
+        else:
+            self.snake = [(self.snake[0][0], self.snake[0][1] + 1)] + self.snake[:-1]
 
+        self.check_move()
+        if costly:
+            self.health -= 1
 
-
-    def move_left(self):
-        if self.food == 0:
-            self.snake = [(self.snake[0][0] - 1, self.snake[0][1])] + self.snake[:-1]
-        if self.food > 0:
+    def move_left(self, costly=True):
+        """Move the snake's head left by one"""
+        if self.digesting > 0:
             self.snake = [(self.snake[0][0] - 1, self.snake[0][1])] + self.snake
-            self.food -= self.food
+            self.digesting -= 1
+        else:
+            self.snake = [(self.snake[0][0] - 1, self.snake[0][1])] + self.snake[:-1]
 
+        self.check_move()
+        if costly:
+            self.health -= 1
 
-
-    def move_right(self):
-        if self.food == 0:
-            self.snake = [(self.snake[0][0] + 1, self.snake[0][1])] + self.snake[:-1]
-        if self.food > 0:
+    def move_right(self, costly=True):
+        """Move the snake's head right by one"""
+        if self.digesting > 0:
             self.snake = [(self.snake[0][0] + 1, self.snake[0][1])] + self.snake
-            self.food -= self.food
+            self.digesting -= 1
+        else:
+            self.snake = [(self.snake[0][0] + 1, self.snake[0][1])] + self.snake[:-1]
 
+        self.check_move()
+        if costly:
+            self.health -= 1
 
+    def check_move(self):
+        """Perform checks after a move has been performed."""
+        if self.has_collision():
+            self.ended = True
 
-    def is_collision(self):
-        if self.snake[0] in self.snake[1:]:
-            return True
-        if 0 > self.snake[0][0]:
-            return True
-        if 0 > self.snake[0][1]:
-            return True
-        if self.width <= self.snake[0][0]:
-            return True
-        if self.height <= self.snake[0][1]:
+        self.eat_apple()
+
+    def has_collision(self):
+        """Check if there has been a collision and return the position of the collision and type"""
+        snake_head = self.snake[0]
+
+        # Self collision of the snake
+        if snake_head in self.snake[1:]:
+            LOGGER.info(f"Self-collision at {snake_head}.")
             return True
 
-    def is_apple(self):
-        if self.snake[0] in self.apples:
+        # Collision with boundary
+        if (
+            snake_head[0] < 0
+            or snake_head[1] < 0
+            or snake_head[0] >= self.width
+            or snake_head[1] >= self.height
+        ):
+            LOGGER.info(f"Boundary collision at {snake_head}")
             return True
+
+        return False
 
     def eat_apple(self):
-        self.apples.remove(self.snake[0])
-        self.food = self.food + 1
-
-    def check(self):
-        if self.is_collision() == True:
-            LOGGER.debug('collision!')
-            return 10
-        if self.is_apple() == True:
-            LOGGER.info('on apple!')
-            self.eat_apple()
+        """Eat the apple if the head just move onto an apple's position"""
+        if self.snake[0] in self.apples:
+            LOGGER.info(f"Ate apple at position {self.snake[0]}.")
+            self.apples.remove(self.snake[0])
+            self.digesting += 1
+            self.health += 10
             self.make_apple()
-            #LOGGER.info('{0}'.format(self.empty_space()))
-
-
 
     def draw(self, win):
-        win.write('O', fgcolor='orange', x=self.snake[0][0], y=self.snake[0][1])
+        win.write("O", fgcolor="orange", x=self.snake[0][0], y=self.snake[0][1])
         if len(self.snake) > 1:
             for segment in self.snake[1:-1]:
-                win.write('#', fgcolor='red', x=segment[0], y=segment[1])
-        win.write('#', fgcolor='yellow', x=self.snake[-1][0], y = self.snake[-1][1])
+                win.write("#", fgcolor="red", x=segment[0], y=segment[1])
+        win.write("#", fgcolor="yellow", x=self.snake[-1][0], y=self.snake[-1][1])
         if len(self.apples) != []:
             for apple in self.apples:
-                win.write('@', fgcolor='green', x=apple[0], y=apple[1])
+                win.write("@", fgcolor="green", x=apple[0], y=apple[1])
 
-        win.colors = ('black','black')
-        win.fill('x',region=(0,self.height,self.width,4))
-        win.colors = ('white','black')
-        win.write('snake length is {0}'.format(len(self.snake)), x=self.width//2 - 9, y=self.height)
+        win.colors = ("black", "black")
+        win.fill("x", region=(0, self.height, self.width, 4))
+        win.colors = ("white", "black")
+        win.write(
+            "snake length is {0}".format(self.snake_length),
+            x=self.width // 2 - 9,
+            y=self.height,
+        )
--- gui.py
+++ gui.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python
+"""Curse GUI for the Snake game"""
+
 import logging
 import sys
-import pygcurse
-import pygame
-from pygame.locals import *
-from game import Game
+
 import coloredlogs
+import pygame
+import pygame.locals as pl
+import pygcurse
+
+from game import Game
 
 LOGGER = logging.getLogger(__name__)
 
@@ -15,43 +20,50 @@ FPS = 40
 
 
 def main():
+    """Main function"""
     log_fmt = "%(asctime)s %(name)s[%(lineno)d] %(levelname)s %(message)s"
-    coloredlogs.install( level = logging.DEBUG,fmt=log_fmt)
-    win = pygcurse.PygcurseWindow(WINWIDTH, WINHEIGHT+4, fullscreen=False)
-    pygame.display.set_caption('Window Title')
+    coloredlogs.install(level=logging.DEBUG, fmt=log_fmt)
+    win = pygcurse.PygcurseWindow(WINWIDTH, WINHEIGHT + 4, fullscreen=False)
+    pygame.display.set_caption("Window Title")
     win.autoupdate = False
     clock = pygame.time.Clock()
 
-    game = Game(WINWIDTH,WINHEIGHT)
+    game = Game(WINWIDTH, WINHEIGHT)
 
-    while True:
-        win.fill(bgcolor='blue')
+    while not game.ended:
+        win.fill(bgcolor="blue")
         handle_events(game)
         game.draw(win)
         win.update()
         clock.tick(FPS)
 
+
 def handle_events(game):
     for event in pygame.event.get():
-        LOGGER.log(5, 'event: {0}'.format(event))
-        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
+        LOGGER.log(5, "event: {0}".format(event))
+
+        # Escape to quit
+        if event.type == pl.QUIT or (
+            event.type == pl.KEYDOWN and event.key == pl.K_ESCAPE
+        ):
             terminate()
-        if event.type == KEYDOWN and (event.key == K_UP or event.key == K_w):
-            game.move_up()
-        if event.type == KEYDOWN and (event.key == K_DOWN or event.key == K_s):
-            game.move_down()
-        if event.type == KEYDOWN and (event.key == K_LEFT or event.key == K_a):
-            game.move_left()
-        if event.type == KEYDOWN and (event.key == K_RIGHT or event.key == K_d):
-            game.move_right()
-        if event.type == KEYDOWN:
-            if game.check() == 10:
-                terminate()
+
+        # Check all other key presses
+        if event.type == pl.KEYDOWN:
+            if event.key == pl.K_UP or event.key == pl.K_w:
+                game.move_up()
+            elif event.key == pl.K_DOWN or event.key == pl.K_s:
+                game.move_down()
+            elif event.key == pl.K_LEFT or event.key == pl.K_a:
+                game.move_left()
+            elif event.key == pl.K_RIGHT or event.key == pl.K_d:
+                game.move_right()
 
 
 def terminate():
     pygame.quit()
     sys.exit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
