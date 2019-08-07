4cea9b22c24e9b98d2801510e12abb34f2017c9c
JP-Ellis <josh@jpellis.me>
2019-08-07 11:54:49 +1000

    Add algorithmic games
    So far, have a (pretty stupid) random walk
    
    Signed-off-by: JP-Ellis <josh@jpellis.me>

---
+++ alogorithm.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+"""Curse GUI for the Snake game"""
+
+import logging
+import random
+import sys
+import time
+
+import coloredlogs
+import pygame
+import pygame.locals as pl
+import pygcurse
+
+from game import Game
+
+LOGGER = logging.getLogger(__name__)
+
+WINWIDTH = 10
+WINHEIGHT = 10
+
+FPS = 100
+
+
+def main():
+    """Main function"""
+    log_fmt = "%(asctime)s %(name)s[%(lineno)d] %(levelname)s %(message)s"
+    coloredlogs.install(level=logging.DEBUG, fmt=log_fmt)
+    win = pygcurse.PygcurseWindow(WINWIDTH, WINHEIGHT + 4, fullscreen=False)
+    pygame.display.set_caption("Window Title")
+    win.autoupdate = False
+    clock = pygame.time.Clock()
+
+    game = Game(WINWIDTH, WINHEIGHT)
+
+    while not game.ended:
+        win.fill(bgcolor="blue")
+        random_walk(game)
+        game.draw(win)
+        win.update()
+        clock.tick(FPS)
+
+    LOGGER.info(f"Final snake length: {game.snake_length}")
+
+
+def random_walk(game):
+    """A nearly random walk.
+
+    If there is an apple, in the immediate vicinity, it will eat it; otherwise,
+    it will just do a random walk until there are no more moves available while
+    avoiding itself and walls.
+
+    """
+    for event in pygame.event.get():
+        LOGGER.log(5, f"event: {event}")
+
+        # Escape to quit
+        if event.type == pl.QUIT or (
+            event.type == pl.KEYDOWN and event.key == pl.K_ESCAPE
+        ):
+            terminate()
+
+    # Get a random direction
+    directions = {
+        (game.snake_head[0] + 1, game.snake_head[1]),
+        (game.snake_head[0] - 1, game.snake_head[1]),
+        (game.snake_head[0], game.snake_head[1] + 1),
+        (game.snake_head[0], game.snake_head[1] - 1),
+    }
+
+    apple_directions = directions & game.apples
+    if apple_directions:
+        LOGGER.debug("Have an apple in the vicinity.")
+        game.move_to(random.sample(apple_directions, 1)[0])
+    else:
+        valid_directions = directions & game.empty_spaces
+        if valid_directions:
+            game.move_to(random.sample(valid_directions, 1)[0])
+        else:
+            LOGGER.warning("No more moves available.")
+            game.ended = True
+
+    # time.sleep(0.1)
+
+
+def terminate():
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
